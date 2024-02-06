405f54e8593f2bf0ddaffd0d3fec540b84da430b
freedomCruz <willycruzo@outlook.com>
2024-02-05 21:53:29 -0500

    Implementación completa del MVC

--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -1,48 +1,14 @@
 import { Router } from 'express'
-
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
 
-moviesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById(id)
-  if (movie) return res.json(movie)
-  return res.status(404).json({ message: 'Movie not found' })
-})
+moviesRouter.get('/:id', MovieController.getById)
 
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-  if (!result.success) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-  const newMovie = await MovieModel.create({ input: result.data })
-  res.status(201).json(newMovie)
-})
+moviesRouter.post('/', MovieController.create)
 
-moviesRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
-  if (!result.success) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-  const { id } = req.params
-  const updatedMovie = await MovieModel.update({ id, input: result.data })
-  return res.json(updatedMovie)
-})
+moviesRouter.patch('/:id', MovieController.update)
 
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  const result = await MovieModel.delete({ id })
-  if (result === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
+moviesRouter.delete('/:id', MovieController.delete)
